b73e9864d586dd60aa78c359cb110b70abd703a7
dana <danotbhd@gmail.com>
2024-09-08 15:13:34 +0300

    err fix

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,8 +1,9 @@
 export default (err, req, res, next) => {
+  const { status = 'fail', statusCode = 500, message = '' } = err;
   console.log(err);
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
+  res.status(statusCode).json({
+    status: status,
+    message: message,
   });
 };
