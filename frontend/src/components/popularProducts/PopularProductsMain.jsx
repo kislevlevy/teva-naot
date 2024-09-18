4b001a173ad7b074d1fb0ea11bff5564665343f1
KobiSat <135111650+KobiSat@users.noreply.github.com>
2024-09-18 13:19:44 +0300

    popular products finished

--- frontend/src/components/popularProducts/PopularProductsMain.jsx
+++ frontend/src/components/popularProducts/PopularProductsMain.jsx
@@ -12,9 +12,9 @@ export default function PopularProducts() {
     AOS.init();
   }, []);
   const [selectedCategory, setSelectedCategory] = useState('');
-  useEffect(() => {
-    // console.log(popularProducts[selectedCategory]);
-  }, [selectedCategory]);
+  // useEffect(() => {
+  //   // console.log(popularProducts[selectedCategory]);
+  // }, [selectedCategory]);
   return (
     <section className="relative pb-24 md:pb-16">
       <div
@@ -22,13 +22,6 @@ export default function PopularProducts() {
         data-aos="fade-up"
         data-aos-duration="2000"
       >
-        <div className="mb-8 w-full">
-          <div className="text-center mb-4">
-            <h2 className="text-4xl font-bold leading-snug text-gray-900 md:text-3xl sm:text-2xl">
-              Popular Products
-            </h2>
-          </div>
-        </div>
         <div className="relative">
           {/* Button Group for small screens */}
           <div className="md:hidden sticky top-0 z-50 bg-white shadow-md">
--- frontend/src/components/popularProducts/PopularProductsSidebar.jsx
+++ frontend/src/components/popularProducts/PopularProductsSidebar.jsx
@@ -1,6 +1,7 @@
 import { Sidebar } from 'flowbite-react';
 import PopularProductsSidbarItem from './PopularProductsSidbarItem';
-import { BsTag, BsStarFill } from 'react-icons/bs';
+import { BsTag } from 'react-icons/bs';
+import { FaRegStar } from "react-icons/fa";
 import { RiHeart2Line } from 'react-icons/ri';
 
 export default function PopularProductsSidebar({ setSelectedCategory }) {
@@ -10,18 +11,18 @@ export default function PopularProductsSidebar({ setSelectedCategory }) {
       <Sidebar.Items className="relative my-1 p-2">
         <Sidebar.ItemGroup className="my-0 py-0">
           <PopularProductsSidbarItem
-            iconToShow={BsStarFill}
-            textToShow="Popular Products"
+            iconToShow={<FaRegStar className='text-emerald-500 mr-2'/>}
+            textToShow="דגמים נבחרים"
             setSelectedCategory={setSelectedCategory}
           />
           <PopularProductsSidbarItem
-            iconToShow={RiHeart2Line}
-            textToShow="Special 4 U"
+            iconToShow={<BsTag className='text-emerald-500 mr-2'/>}
+            textToShow="הנמכרים ביותר"
             setSelectedCategory={setSelectedCategory}
           />
-          <PopularProductsSidbarItem
-            iconToShow={BsTag}
-            textToShow="New Collection"
+           <PopularProductsSidbarItem
+            iconToShow={<RiHeart2Line className='text-emerald-500 mr-2'/>}
+            textToShow="במיוחד בשבילך"
             setSelectedCategory={setSelectedCategory}
           />
         </Sidebar.ItemGroup>
